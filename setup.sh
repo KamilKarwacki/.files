e6f853cbed5698a4d9302f5833e796fa605e4972
KamilKarwacki <kamil.karwacki@rwth-aachen.de>
2022-12-22 02:31:26 +0100

    changes to setup

--- setup.sh
+++ setup.sh
@@ -4,35 +4,51 @@
 # distro and clone all required repos and install all software to create my personal development
 # enviroment
 
-cd ~
+if ! command -v curl &> /dev/null
+then
+	sudo apt-get install curl
+fi
+
+cd ${HOME} 
 sudo apt-get update -qq
 
 # first some basic utilities that are needed
-apt-get install git cmake
+sudo apt-get install git cmake
 # regolith specific things
-apt-get install regolit-look-*
+sudo apt-get install regolit-look-*
 
 # lets start with neovim 
 # first lets create a software folder and clone git 
-mkdir software && cd software
-wget https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz
-tar -xvf nvim-linux61.tar.gz
-mv nvim-linux61 nvim 
-cd ..
-echo "# neovim path" >> .bashrc
-echo "${HOME}/software/nvim" >> .bashrc
+
+if [ ! -d "software/nvim" ] 
+then
+
+	mkdir software 
+	cd software
+	wget https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz
+	tar -xvf nvim-linux64.tar.gz
+	mv nvim-linux64 nvim 	
+	cd ..
+	echo "# neovim path" >> .bashrc
+	echo "export PATH=\$PATH:\${HOME}/software/nvim/bin" >> .bashrc
+  echo "alias vim=nvim" >> .bashrc
+	source .bashrc
+fi
 
 # now get my dotfiles and create a symlink to .config
-git clone git@github.com:KamilKarwacki/.files.git
 ln -s ${HOME}/.files/nvim ${HOME}/.config
 
 # clone wallpaper repo and put into .wallpapers
-git clone git@github.com:KamilKarwacki/wallpapers.git
-mv wallpapers .wallpapers
+if [ ! -d ".wallpapers" ] 
+then
+	git clone git@github.com:KamilKarwacki/wallpapers.git
+	mv wallpapers .wallpapers
+fi
+#
 
 # next the regolith stuff 
 # install picom
-apt install regolith-compositor-picom-glx
+sudo apt install regolith-compositor-picom-glx
 # create symlink from dotfiles
 ln -s ${HOME}/.files/regolith2 ${HOME}/.config
 # and maybe refresh the look
@@ -40,8 +56,9 @@ regolith-look refresh
 
 
 # lets try to install rust
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+#curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 # add path to bashrc
-cat "export PATH=\$PATH:${HOME}/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/bin" >> .bashrc
+#echo "export PATH=\$PATH:\${HOME}/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/bin" >> .bashrc
+#source .bashrc
 # install bacon
-cargo install --locked bacon
+#cargo install --locked bacon
