039a097346d9875636ab8dc206c27e8b2205dda8
Kamil Karwacki <kamilk@miltenyi.com>
2024-02-08 15:28:02 +0100

    easy way to get to config files

--- nvim/lua/limak/remap.lua
+++ nvim/lua/limak/remap.lua
@@ -2,6 +2,11 @@ local nnoremap = require("limak.keymap").nnoremap
 -- opens netrw which is the buintin file explorer
 nnoremap("<leader>pv", "<cmd>Ex<CR>")
 
+-- go to root dir of config
+vim.cmd([[command! Conf tabnew ~/.config/nvim/ | Ex]])
+
+
+
 nnoremap("<leader>te", ":tabedit <cr>")
 
 -- no need to use shift ; to go into command mode 
