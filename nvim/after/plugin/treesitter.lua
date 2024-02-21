15b6e0e000f286201e8ba712933ad3081c20c26f
Kamil Karwacki <kamilk@miltenyi.com>
2024-02-21 16:18:28 +0100

    tmux using mouse, treesitter added syntax highlighting

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -15,7 +15,7 @@ require'nvim-treesitter.configs'.setup {
   highlight = {
     -- `false` will disable the whole extension
     enable = true,
-    
+
     disable = function(lang, bufnr)
       return lang == "cpp" and vim.api.nvim_buf_line_count(bufnr) > 10000
     end,
@@ -27,3 +27,5 @@ require'nvim-treesitter.configs'.setup {
     additional_vim_regex_highlighting = false,
   },
 }
+
+vim.cmd('autocmd BufRead,BufNewFile *.icc set filetype=cpp')
