c9fefd558587d677bc33980aa9536eb5657ea032
KamilKarwacki <karwacki@roofline.ai>
2025-07-03 15:39:47 +0200

    added further things to install that were in setup

--- install.sh
+++ install.sh
@@ -3,3 +3,14 @@ echo "running install.sh"
 pwd
 ln -s -f /home/KamilKarwacki/.config/coderv2/dotfiles/nvim /home/KamilKarwacki/.config/
 
+if [ ! -f "./.local/share/nvim/site/pack/packer/start/packer.nvim" ]; then
+  git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+   ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+fi
+
+sudo apt-get install tmux ripgrep
+#create symbolic link for tmux
+if [ ! -L "/home/KamilKarwacki/.tmux.config" ]; then
+  ln -s /home/KamilKarwacki/.config/coderv2/dotfiles/nvim/tmux.config /home/KamilKarwacki/.tmux.config
+fi
+
