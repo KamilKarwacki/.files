85b2d085ed44590f57ce9475899e5f42e87ba08c
KamilKarwacki <karwacki@roofline.ai>
2026-01-20 09:32:02 +0100

    vi mode for fish

--- fish/config.fish
+++ fish/config.fish
@@ -12,6 +12,9 @@ alias gits "git status"
 set -x PATH $PATH $HOME/software/Odin
 set -x PATH $PATH $HOME/software/zig
 set -x PATH $PATH $HOME/software/FlameGraph
+set -x PATH $PATH $HOME/scripts/
 set -x PATH $PATH /home/kamil/fun/compilers/llvm-project/build/bin
 
 eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv | source)
+
+fish_vi_key_bindings
